370ea42aadd94429d4c322d070ca3357f24a8e44
Gevrai J-Tremblay <gevrai@gmail.com>
2017-04-06 12:07:48 -0400

    First compilable and runnable!

---
+++ DM3/src/Main.java
@@ -0,0 +1,8 @@
+import Application.*;
+
+public class Main { 
+	public static void main(String args[]) { 
+		@SuppressWarnings("unused")
+		Application a = new Application(args);
+	} 
+} 
---
+++ DM3/src/UI/UserFrame.java
@@ -0,0 +1,20 @@
+package UI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+import Sessions.Session;
+
+public class UserFrame extends JFrame{
+
+	JButton logout_button = new JButton("Logout");
+	
+	/**
+	 * 
+	 * @param s
+	 */
+	public boolean logout(Session s) {
+		// TODO - implement HomeFrame.logout
+		throw new UnsupportedOperationException();
+	}
+}
