02dde791100d02359becb6e10324f4b68e981337
Gevrai J-Tremblay <gevrai@gmail.com>
2017-04-07 16:34:06 -0400

    Forms are coherent now (normally), added helper functions in others

--- DM3/src/Travel/Forms/TravelFacilityForm.java
+++ DM3/src/Travel/Forms/TravelFacilityForm.java
@@ -1,16 +1,37 @@
 package Travel.Forms;
 
+import Travel.TravelType;
 import Travel.Facilities.TravelFacility;
 
 public class TravelFacilityForm extends BaseForm {
 
+	private String id;
 	private String location;
 	private String name;
+	private TravelType type;
 
 	public TravelFacilityForm(TravelFacility tf) {
+		this.ID = tf.getId();
 		this.location = tf.getLocation();
 		this.name = tf.getName();
+		this.type = tf.getType();
 	}
+
+	public String getLocation() { return this.location; }
+
+	public void setLocation(String location) { this.location = location; }
+
+	public String getName() { return this.name; }
+
+	public void setName(String name) { this.name = name; }
+
+	public String getId() { return id; }
+
+	public void setId(String id) { this.id = id; }
+
+	public TravelType getType() { return type; }
+
+	public void setType(TravelType type) { this.type = type; }
 
 	public BaseForm createFromPrompt() {
 		// TODO - implement TravelFacilityForm.createFromPrompt
@@ -20,22 +41,6 @@ public class TravelFacilityForm extends BaseForm {
 	public void modifyFromPrompt() {
 		// TODO - implement TravelFacilityForm.modifyFromPrompt
 		throw new UnsupportedOperationException();
-	}
-
-	public String getLocation() {
-		return this.location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 }
