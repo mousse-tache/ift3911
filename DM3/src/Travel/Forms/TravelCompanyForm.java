f5428bc786c404af92e3f1c03de23e1faf87d544
Gevrai <gevrai@iMac-de-Sylvie.local>
2017-04-07 16:34:06 -0400

    Forms are coherent now (normally), added helper functions in others

--- DM3/src/Travel/Forms/TravelCompanyForm.java
+++ DM3/src/Travel/Forms/TravelCompanyForm.java
@@ -1,13 +1,47 @@
 package Travel.Forms;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Travel.ConcreteVehicle;
 import Travel.TravelCompany;
+import Utils.Searcher.Searcher;
 
 public class TravelCompanyForm extends BaseForm {
 
+	private String companyID;
 	private String name;
+	private List<String> vehiclesID;
 
-	public TravelCompanyForm(TravelCompany tc) {
+	public TravelCompanyForm(TravelCompany tc) { 
 		this.name = tc.getName();
+		this.companyID = tc.getID();
+		this.vehiclesID = new ArrayList<String>();
+		tc.getVehicles().forEach(v -> vehiclesID.add(v.getId()));
+	}
+
+	public String getName() { return this.name; }
+
+	public void setName(String name) { this.name = name; }
+	
+	public TravelCompany getCompany() { return Searcher.getCompanyFromID(this.companyID); }
+	public String getCompanyID() { return this.companyID; }
+
+	public void setCompany(TravelCompany company) { this.companyID = company.getID(); }
+	public void setCompany(String companyID) { this.companyID = companyID; }
+
+	public List<String> getVehiclesID() { return vehiclesID; }
+	public List<ConcreteVehicle> getVehicles() { 
+		List<ConcreteVehicle> vehicles = new ArrayList<ConcreteVehicle>();
+		// TODO check if ID exists
+		vehiclesID.forEach(id -> vehicles.add(Searcher.getVehicleFromID(id)));
+		return vehicles;
+	}
+
+	public void setVehiclesID(List<String> vehiclesID) { this.vehiclesID = vehiclesID; }
+	public void setVehicles(List<ConcreteVehicle> vehicles) { 
+		vehiclesID = new ArrayList<String>();
+		vehicles.forEach(v -> vehiclesID.add(v.getId()));
 	}
 
 	public BaseForm createFromPrompt() {
@@ -20,12 +54,5 @@ public class TravelCompanyForm extends BaseForm {
 		throw new UnsupportedOperationException();
 	}
 
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 }
