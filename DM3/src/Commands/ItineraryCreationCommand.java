a5c9f9b1720e4576a22d31b4e78528f8d6c60684
Charles Langlois <schok53@gmail.com>
2017-04-09 14:59:31 -0400

    Modif du diagramme; un peu de codage des commandes

--- DM3/src/Commands/ItineraryCreationCommand.java
+++ DM3/src/Commands/ItineraryCreationCommand.java
@@ -1,4 +1,21 @@
 package Commands;
 
 public class ItineraryCreationCommand extends AdminCreationCommand {
+    boolean execute(){
+        this.result = CreationMediator.createTrip(this.args);
+        if (this.result) {
+            this.status = CommandStatus.success;
+            return true;
+        } else {
+            this.status = CommandStatus.failure;
+            return false;
+        }
+    }
+    boolean unexecute(){
+        if (this.status == CommandStatus.success){
+            return DeletionMediator.deleteTrip(this.result);//this.result devrait être l'id du trip créé.
+        } else {
+            return false;
+        }
+    }
 }
--- DM3/src/Commands/VehicleModificationCommand.java
+++ DM3/src/Commands/VehicleModificationCommand.java
@@ -1,4 +1,16 @@
 package Commands;
 
 public class VehicleModificationCommand extends AdminModificationCommand {
+    boolean execute(){
+        this.rollbackForm = ModificationMediator.createModificationForm(this.getID());
+        return ModificationMediator.modifyVehicle(this.args);//le résultat d'une modification est un booléen
+    }
+    boolean unexecute(){
+        if (this.status == CommandStatus.success){
+            return ModificationMediator.modifyVehicle(this.rollbackForm);
+        } else {
+            return false;
+        }
+    }
+
 }
